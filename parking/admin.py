4de765472ed300696f99fae6041c1797c3a75c0e
doebkblcya <2738103877@qq.com>
2024-11-26 14:32:57 +0800

    day5

--- parking/admin.py
+++ parking/admin.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Vehicle #用于存储停车场中车辆的基本信息和状态。
+from .models import ParkingSettings
 
 admin.site.register(Vehicle)
+admin.site.register(ParkingSettings)
